5c4fc95c6020e8fe549dfa7c608d5e4c9c3d28b7
samheartache <20imas07@gmail.com>
2025-07-14 23:57:28 +0300

    Validate user'ss inputs while registrating

--- register_handlers.py
+++ register_handlers.py
@@ -5,6 +5,7 @@ from aiogram.fsm.context import FSMContext
 
 import utils
 import keyboards as kb
+import validators
 from states import Register
 
 router = Router()
@@ -24,60 +25,88 @@ async def start_reg(callback: CallbackQuery, state=FSMContext):
 
 @router.message(Register.sex)
 async def handle_sex(message: Message, state: FSMContext):
-    if message.text == 'Мужской':
-        await state.update_data(sex=0)
-    else:
-        await state.update_data(sex=1)
+    if validators.sex_validate(message.text):
+        if message.text == 'Мужской':
+            await state.update_data(sex=0)
+        else:
+            await state.update_data(sex=1)
 
-    await state.set_state(Register.search_desire)
-    await message.answer('Чьи анкеты вы хотитите смотреть?', reply_markup=kb.search_desire)
+        await state.set_state(Register.search_desire)
+        await message.answer('Чьи анкеты вы хотитите смотреть?', reply_markup=kb.search_desire)
+        return
+    await message.answer('Введите пол корректно', reply_markup=kb.sex_choice)
+    return
 
 
 @router.message(Register.search_desire)
 async def handle_search_desire(message: Message, state: FSMContext):
-    if message.text == 'Парней':
-        await state.update_data(search_desire=0)
-    elif message.text == 'Девушек':
-        await state.update_data(search_desire=1)
-    else:
-        await state.update_data(search_desire=2)
-    
-    await state.set_state(Register.name)
-    await message.answer('Введите ваше имя')
+    if validators.search_desire_validate(message.text):
+        if message.text == 'Парней':
+            await state.update_data(search_desire=0)
+        elif message.text == 'Девушек':
+            await state.update_data(search_desire=1)
+        else:
+            await state.update_data(search_desire=2)
+        
+        await state.set_state(Register.name)
+        await message.answer('Введите ваше имя')
+        return
+    await message.answer('Выберите корректно.\nЧьи анкеты вы хотитите смотреть?', reply_markup=kb.search_desire)
+    return
 
 
 @router.message(Register.name)
 async def handle_name(message: Message, state: FSMContext):
-    await state.update_data(name=message.text)
-    await state.set_state(Register.age)
-    await message.answer('Введите ваш возраст')
+    if validators.name_validate(message.text):
+        await state.update_data(name=message.text)
+        await state.set_state(Register.age)
+        await message.answer('Введите ваш возраст')
+        return 
+    await message.answer('Введите имя корректно (до 20 символов)')
+    return
 
 
 @router.message(Register.age)
 async def handle_age(message: Message, state: FSMContext):
-    await state.update_data(age=int(message.text))
-    await state.set_state(Register.city)
-    await message.answer('Введите ваш город')
+    if validators.age_validate(message.text):
+        await state.update_data(age=int(message.text))
+        await state.set_state(Register.city)
+        await message.answer('Введите ваш город')
+        return
+    await message.answer('Введите возраст корректно')
+    return 
 
 
 @router.message(Register.city)
 async def handle_city(message: Message, state: FSMContext):
-    await state.update_data(city=message.text)
-    await state.set_state(Register.description)
-    await message.answer('Напишите описание для своей анкеты')
+    if validators.city_validate(message.text):
+        await state.update_data(city=message.text)
+        await state.set_state(Register.description)
+        await message.answer('Напишите описание для своей анкеты')
+        return
+    await message.answer('Введите город корректно')
+    return
 
 
 @router.message(Register.description)
 async def handle_description(message: Message, state: FSMContext):
-    await state.update_data(description=message.text)
-    await state.set_state(Register.photo)
-    await message.answer('Отправьте фото для вашей анкеты')
+    if validators.description_validate(message.text):
+        await state.update_data(description=message.text)
+        await state.set_state(Register.photo)
+        await message.answer('Отправьте фото для вашей анкеты')
+        return
+    await message.answer('Введите описание корректно')
+    return
 
 
 @router.message(Register.photo)
 async def handle_photo(message: Message, state: FSMContext):
-    await state.update_data(photo=message.photo[-1].file_id)
-    await send_profile(message=message, state=state)
+    if validators.photo_validate(message.photo):
+        await state.update_data(photo=message.photo[-1].file_id)
+        await send_profile(message=message, state=state)
+        return
+    await message.answer('Отправьте фото корректно')
+    return
 
 
 async def send_profile(message: Message, state: FSMContext):
---
+++ validators.py
@@ -0,0 +1,37 @@
+def sex_validate(data: str | None):
+    return data in ('Мужской', 'Женский')
+
+
+def search_desire_validate(data: str | None):
+    return data in ('Девушек', 'Парней', 'Не важно')
+
+
+def name_validate(data: str | None):
+    if data:
+        return len(data) <= 20
+    return False
+
+
+def age_validate(data: str | None):
+    if data:
+        if data.isdigit():
+            return 10 <= int(data) <= 100
+        return False
+    return False
+
+
+def city_validate(data: str | None):
+    if data:
+        return len(data) <= 25 and data.isalpha()
+    return False
+
+
+def description_validate(data: str | None):
+    if data:
+        return len(data) <= 500
+    return False
+
+
+def photo_validate(data):
+    return data
+
